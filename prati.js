f23e19ceff8b662f9791300b2d3419f845e25acf
Ayara-Sharma <sharmamunnu555@gmail.com>
2023-04-05 18:55:51 -0700

    first commit

---
+++ prati.js
@@ -0,0 +1,6 @@
+// let data:boolean = true  ;
+// console.log(data);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(2, 7));
---
+++ prati.ts
@@ -0,0 +1,9 @@
+// let data:boolean = true  ;
+// console.log(data);
+
+
+function add(num1:number,num2:number)
+{
+    return num1+num2;   
+}
+console.log(add(2,7));
